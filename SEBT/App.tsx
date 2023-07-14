91a058c798775c8eb1561832f007ab044ce6d4cc
haris18896 <haris18896@gmail.com>
2023-07-15 01:42:39 +0500

    webview is working fine in metro, but on build it shows white screen

--- SEBT/App.tsx
+++ SEBT/App.tsx
@@ -1,21 +1,55 @@
-import React, {useRef} from 'react';
-
-// ** webview
+import React, {useState} from 'react';
+import {View, ActivityIndicator, StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
 
-function App() {
-  const webviewRef = useRef(null);
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const onMessage = event => {
+    // Handle WebView messages here
+    console.log('Received message from WebView:', event.nativeEvent.data);
+  };
 
   return (
-    <WebView
-      source={{uri: 'http://bms.tracking.me/login'}}
-      originWhitelist={['*']}
-      ref={webviewRef}
-      javaScriptEnabled={true}
-      onLoad={() => console.log('WebView loaded')}
-      onError={error => console.log('WebView error:', error)}
-    />
+    <View style={styles.container}>
+      {isLoading && (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator color="white" />
+        </View>
+      )}
+      <WebView
+        source={{uri: 'http://bms.tracking.me/login'}}
+        startInLoadingState={true}
+        renderLoading={() => null}
+        style={styles.webview}
+        onLoad={() => {
+          setIsLoading(false);
+        }}
+        onMessage={onMessage}
+      />
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  loaderContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'black',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  webview: {
+    flex: 1,
+  },
+});
 
 export default App;
