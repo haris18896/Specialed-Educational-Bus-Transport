3792e9da6541dc4ebfaa86fe24bc44c93c751bc8
haris18896 <haris18896@gmail.com>
2023-07-15 18:00:06 +0500

    the above is not checking the permission if its allowed or not

--- SEBT/android/app/src/main/java/com/sebt/BootReceiver.java
+++ SEBT/android/app/src/main/java/com/sebt/BootReceiver.java
@@ -1,44 +1,17 @@
 package com.sebt;
 
-import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
-import android.widget.Toast;
-import android.app.KeyguardManager;
-
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
-
-    @Override
+  @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            // Request SYSTEM_ALERT_WINDOW permission if needed
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(context)) {
-                Toast.makeText(context, "Please enable the 'Display over other apps' permission for this app.", Toast.LENGTH_LONG).show();
-                Intent permissionIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + context.getPackageName()));
-                permissionIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(permissionIntent);
-            }
-
-            // Request DISABLE_KEYGUARD permission if needed
-            KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
-            if (keyguardManager != null && keyguardManager.isKeyguardLocked()) {
-                Intent permissionIntent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                permissionIntent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, new ComponentName(context, YourDeviceAdminReceiver.class));
-                permissionIntent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Enable this permission to disable the keyguard.");
-                permissionIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(permissionIntent);
-            }
-
-            // Your other code for handling the boot completion event
-            Intent myIntent = new Intent(context, MainActivity.class);
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(myIntent);
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent activityIntent = new Intent(context, MainActivity.class);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(activityIntent);
         }
     }
 }
