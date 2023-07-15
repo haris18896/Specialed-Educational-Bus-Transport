21cc17c32141f69277b9024810d4322a7ff12d46
haris18896 <haris18896@gmail.com>
2023-07-15 05:36:38 +0500

    alert added

--- SEBT/App.tsx
+++ SEBT/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Platform} from 'react-native';
+import {Platform, Alert, Linking} from 'react-native';
 import {WebView} from 'react-native-webview';
 import * as Permissions from 'react-native-permissions';
 
@@ -7,12 +7,29 @@ async function requestOverlayPermission() {
   if (Platform.OS === 'android') {
     try {
       const permissionStatus = await Permissions.check('overlay');
-      if (permissionStatus === 'granted') {
-        console.log('Display over other apps permission granted');
+      if (permissionStatus !== 'granted') {
+        const rationale =
+          'We need permission to display over other apps in order to provide a better user experience. Please grant the permission in the next prompt.';
+        const permissionRequest = await Permissions.request('overlay', {
+          rationale,
+        });
+
+        if (permissionRequest === 'granted') {
+          console.log('Display over other apps permission granted');
+        } else {
+          console.log('Display over other apps permission denied');
+          // Display an alert or perform additional actions if needed
+          Alert.alert(
+            'Permission Blocked',
+            'Please grant the "Display over other apps" permission in the app settings. If you already have the permissions press "Cancel"',
+            [
+              {text: 'Cancel', style: 'cancel'},
+              {text: 'Open Settings', onPress: () => Linking.openSettings()},
+            ],
+          );
+        }
       } else {
-        console.log('Display over other apps permission denied');
-        // You can open the app settings to allow the permission
-        Permissions.openSettings();
+        console.log('Display over other apps permission already granted');
       }
     } catch (err) {
       console.warn('Error checking permission:', err);
